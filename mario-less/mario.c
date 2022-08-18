04289a7a9df346d460499bc6832581890f5a8cff
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-08-18 22:53:26 +0000

    Thu, Aug 18, 2022, 10:53 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,6 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
-void printer(int x ,int y);
+void printer(int x, int y);
 int main(void)
 {
     while (1)
@@ -8,29 +8,32 @@ int main(void)
         int num = get_int("Enter the Hight: ");
         if (num < 9 && num > 0)
         {
-            printer(num,0);
+            printer(num, 0);
             break;
         }
     }
 }
 
-void printer(int x ,int y)
+void printer(int x, int y)
 {
-    if (x  != 0)
+    if (x != 0)
     {
         x--;
         y++;
-       // printf("%i %i",x,y);
-        for (int i = x; i  != 0; i--){
+        // printf("%i %i",x,y);
+        for (int i = x; i != 0; i--)
+        {
             printf(" ");
         }
-        for (int z = y; z  != 0; z--){
+        for (int z = y; z != 0; z--)
+        {
             printf("#");
         }
         printf("\n");
-        printer( x, y);
-
+        printer(x, y);
     }
-    else{
-    return;}
+    else
+    {
+        return;
+    }
 }
