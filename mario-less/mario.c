11ab7ac0a48603db20e9233e985fe27534dab4da
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-08-18 22:06:21 +0000

    Thu, Aug 18, 2022, 10:06 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -3,11 +3,12 @@
 
 int main(void)
 {
-while(1){
-   int num = get_int("Enter the Hight: ");
-    if(num <9 && num > 0){
-        printf("hello world\n");
+    while (1)
+    {
+        int num = get_int("Enter the Hight: ");
+        if (num < 9 && num > 0)
+        {
+            printf("hello world\n");
+        }
     }
-}
-
 }
