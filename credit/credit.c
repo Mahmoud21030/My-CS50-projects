50a0a768a212375cefd79b96dacb437db1f6ac97
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-08-19 01:43:16 +0000

    Fri, Aug 19, 2022, 1:43 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -58,7 +58,12 @@ int sumf(int x)
 {
     int sum = 0;
     int l = getlen(x);
-    
+    while (x != 0)
+   {
+      remainder = x % 10;
+      sum       = sum + remainder;
+      t         = x / 10;
+   }
 
  printf("%i\n", sum);
     return sum;
