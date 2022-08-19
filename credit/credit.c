b790377c60a5cbef311181caf4d4c00670ad837b
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-08-19 01:07:19 +0000

    Fri, Aug 19, 2022, 1:07 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -22,12 +22,12 @@ void checksum(int x)
 {
     int sum = 0;
     int l = getlen(x);
-    for (int i = l - 2; i <= 0; i -= 2)
+    for (int i = l - 2; i >= 0; i -= 2)
     {
         sum += array[i];
-        printf("%i\n", sum);
-    }
 
+    }
+ printf("%i\n", sum);
     return;
 }
 
