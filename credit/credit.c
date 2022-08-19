2ee3d5fd2c38e78fb7fd5f9609b9e29d702ae205
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-08-19 00:50:25 +0000

    Fri, Aug 19, 2022, 12:50 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -9,35 +9,39 @@ int getlen(int y);
 void digitize(int card);
 int array[16];
 
-
 int main(void)
 {
     int card = get_int("enter Card number");
-    checksum(card);
+
     digitize(card);
-    printf("%i\n",array[1]);
-
-
+    printf("%i\n", array[0]);
+    checksum(card);
 }
 
 void checksum(int x)
 {
+    int l = getlen(card);
 
     return;
 }
 
-int getlen(int y){
-    if (y<1 )
-    return 1;
-    else{
-    return ceil(log10(y+0.0001));}
+
+int getlen(int y)
+{
+    if (y < 1)
+        return 1;
+    else
+    {
+        return ceil(log10(y + 0.0001));
+    }
 }
 
-void digitize(int card){
+void digitize(int card)
+{
     int l = getlen(card);
-    for(int i =0; i< l;i++){
-        array[i]=card/pow(10,(l-i+1));
-        card=card-array[i]*pow(10,(l-i+1));
+    for (int i = 0; i < l; i++)
+    {
+        array[i] = card / pow(10, (l - i - 1));
+        card = card - array[i] * pow(10, (l - i - 1));
     }
-
 }
