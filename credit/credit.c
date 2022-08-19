1caffadceb343a220c8724ef326a93ba64ba6e25
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-08-19 00:08:10 +0000

    Fri, Aug 19, 2022, 12:08 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#define NELEMS(x)  (sizeof(x) / sizeof((x)[0]))
 
 void checksum(int x);
 int array[16];
@@ -9,7 +10,7 @@ int main(void)
     int card = get_int("enter Card number");
     checksum(card);
     string x = "90";
-    printf("%i",x[0]);
+    printf("%i",NELEMS(array));
 }
 
 void checksum(int x)
