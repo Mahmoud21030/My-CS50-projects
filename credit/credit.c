accb9d28d295f9e4dd6bf10a8214f1141c0f63ce
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-08-19 00:20:22 +0000

    Fri, Aug 19, 2022, 12:20 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,8 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
-#define NELEMS(x)  (sizeof(x) / sizeof((x)[0]))
+#include <math.h>
+
+#define NELEMS(x) (sizeof(x) / sizeof((x)[0]))
 
 void checksum(int x);
+int getlen(int y);
+
 int Mas[16];
 int Am[15];
 int vis16[16];
@@ -13,10 +17,15 @@ int main(void)
     int card = get_int("enter Card number");
     checksum(card);
 
+    printf("%i",getlen(card));
 }
 
 void checksum(int x)
 {
 
-return;
+    return;
+}
+
+int getlen(int y){
+    return ceil(log(y));
 }
