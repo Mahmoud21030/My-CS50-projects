6b27198a74c6b712d4f6f395dec39a76f0ad55a1
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-10 22:59:04 +0000

    Sat, Sep 10, 2022, 10:59 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -20,14 +20,14 @@ int main(int argc, string argv[])
 
     for (int i = 0, n = strlen(argv[1]); i < n; i++) // to lowercase char
     {
-       (argv[1])[i] = tolower(argv[1][i]);
+       (argv[1])[i] = tolower((argv[1])[i]);
         (argv[1])[i] -= 97; // mapping Alpha into 0.1.2
     }
 
     for (int i = 0, n = strlen(word); i < n; i++) // to lowercase char
     {
         word[i] = tolower(word[i]);
-       word[i] = argv[1][word[i] - 97]; // mapping Alpha into 0.1.2
+       word[i] = (argv[1])[word[i] - 97]; // mapping Alpha into 0.1.2
 
     }
 
