5f8c710ff1225c9fe1fe61df4843a26075bbbfe3
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-10 21:19:59 +0000

    Sat, Sep 10, 2022, 9:19 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -2,40 +2,49 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#define isalphab(word) (isalpha(word)&&1)
+#define isalphab(word) (isalpha(word) && 1)
 int check(string code, int count);
 char Sub[26] = "abcdefghijklmnopqrstuvwxyz";
 
 int main(int argc, string argv[])
-{   argv[1]="YTNSHKVEFXRBAUQZCLWDMIPGJ";
-    argc =2;
-    int x = check(argv[1], argc);
-}
-
-int check(string code, int count)
 {
-    int c = 0;
-    if (count == 2) // check if there is number or no
-    {
-        if (strlen(code) == 26) // check 26 char
-        {
-            for (int i = 0; i < 26; i++) // check all alpha
-            {
-                c += isalphab(code[i]);
+    argv[1] = "YTNSHKVEFXRBAUQZCLWDMIPGJ";
+    argc = 2;
+    n = strlen(argv);
 
-            }
-            printf("%d",c);
-        }
-        else
+    if (argc == 2) // check if there is number or no
+    {///////////////////////////////////////////////////////////////////////////
+
+        if (n == 26)
         {
             printf("Key must contain 26 characters.");
 
-            printf("%i %s\n", count, code);
+
         }
+        else {
+            printf("Key must contain 26 characters.");
+        }
+
+
+
     }
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////
     else // For null input
     {
         printf("Usage: ./substitution key");
     }
     return 0;
+}
+
+int numalpha(string code, int count)
+{
+    for (int i = 0; i < 26; i++) // check all alpha
+    {
+        c += isalphab(code[i]);
+    }
+    return c;
 }
