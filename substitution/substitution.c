8ff908ccc13dd6566533e6470a4ec91c35b629a5
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-10 19:22:06 +0000

    Sat, Sep 10, 2022, 7:22 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,11 +3,18 @@
 #include <string.h>
 #include <ctype.h>
 
+int Sub[26]=;
+
 int main(int argc, string argv[])
 {
     if (argc == 2) // check if there is number or no
     {
-        if (strlen(argv[1]) < 26)
+        if (strlen(argv[1]) == 26)
+        {
+            for(int=)
+
+        }
+        else
         {
             printf("Key must contain 26 characters.");
 
