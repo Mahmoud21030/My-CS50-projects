9b01c866b7eab278a691998f2f21f43f30345eb7
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-10 21:32:47 +0000

    Sat, Sep 10, 2022, 9:32 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -4,45 +4,15 @@
 #include <ctype.h>
 #define isalphab(word) (isalpha(word) && 1)
 int numalpha(string code);
+int check(int argc, string argv);
 char Sub[26] = "abcdefghijklmnopqrstuvwxyz";
 
 int main(int argc, string argv[])
 {
     argv[1] = "YTNSHKVEFXRBAUQZCLWDMIPGJO";
     argc = 2;
-   int n = strlen(argv[1]);
-   int na = numalpha(argv[1]);
-    if (argc == 2) // check if there is number or no
-    {              ///////////////////////////////////////////////////////////////////////////
-        if (na > 0)
-        {
-            if (n == 26)
-            {
-                if (na == 26)
-                {
-                }
-                else
-                {
-                    printf("Key must contain 26 characters.");
-                }
-            }
-            else
-            {
-                printf("Key must contain 26 characters.");
-            }
-        }
-        else // For number and alpha
-        {
-            printf("Usage: ./substitution key");
-        }
-    }
-
-    ////////////////////////////////////////////////////////////////////////////
-    else // For null input
-    {
-        printf("Usage: ./substitution key");
-    }
-    return 0;
+    int x = check( argc,  argv[1]);
+    printf("%d",x);
 }
 
 int numalpha(string code){
@@ -52,4 +22,45 @@ int numalpha(string code){
         c += isalphab(code[i]);
     }
     return c;
+}
+
+int check(int argc, string argv){
+    int n = strlen(argv[1]);
+   int na = numalpha(argv[1]);
+    if (argc == 2) // check if there is number or no
+    {              ///////////////////////////////////////////////////////////////////////////
+        if (na > 0)
+        {
+            if (n == 26)
+            {
+                if (na == 26)
+                {
+                    return 1;
+                }
+                else
+                {
+                    printf("Key must contain 26 characters.");
+                    return 0;
+                }
+            }
+            else
+            {
+                printf("Key must contain 26 characters.");
+                return 0;
+            }
+        }
+        else // For number and alpha
+        {
+            printf("Usage: ./substitution key");
+            return 0;
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    else // For null input
+    {
+        printf("Usage: ./substitution key");
+        return 0;
+    }
+
 }
