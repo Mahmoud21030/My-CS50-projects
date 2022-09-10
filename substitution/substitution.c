93994dee5d7ed736c3655842f68757129cf1d161
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-10 18:18:00 +0000

    Sat, Sep 10, 2022, 6:18 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,8 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
+    if (argc == 2) // check if there is number or no
+    {
 
+        printf("%i %s\n", argc, argv[1]);
+    }
+    else //For null input
+    {
+        printf("Usage: ./substitution key");
+    }
 }
