15588017a3850e4b3db6a8a75c0a1722d735fa58
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-10 19:30:22 +0000

    Sat, Sep 10, 2022, 7:30 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,24 +3,21 @@
 #include <string.h>
 #include <ctype.h>
 
-int Sub[26]={a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,f,w,x,y,z};
+int Sub[26] = {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, f, w, x, y, z};
 
 int main(int argc, string argv[])
 {
-
-
 }
 
-
-
-int check(string code,int count){
+int check(string code, int count)
+{   int c=0;
     if (argc == 2) // check if there is number or no
     {
-        if (strlen(code) == 26) //check 26 char
-        { //check all alpha
-            for(counter)
-
-
+        if (strlen(code) == 26) // check 26 char
+        {
+            for (int i = 0 ; i < 26; i++)  // check all alpha
+            {
+            }
         }
         else
         {
@@ -33,5 +30,4 @@ int check(string code,int count){
     {
         printf("Usage: ./substitution key");
     }
-
 }
