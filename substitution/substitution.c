137d8309ecef4b14e1b697a3a37c60490257e613
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-10 19:26:27 +0000

    Sat, Sep 10, 2022, 7:26 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -7,22 +7,30 @@ int Sub[26]={a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,f,w,x,y,z};
 
 int main(int argc, string argv[])
 {
+
+
+}
+
+
+
+int check(string code,int count){
     if (argc == 2) // check if there is number or no
     {
-        if (strlen(argv[1]) == 26)
+        if (strlen(code) == 26)
         {
-            
+
 
         }
         else
         {
             printf("Key must contain 26 characters.");
 
-            printf("%i %s\n", argc, argv[1]);
+            printf("%i %s\n", count, code);
         }
     }
     else // For null input
     {
         printf("Usage: ./substitution key");
     }
+
 }
