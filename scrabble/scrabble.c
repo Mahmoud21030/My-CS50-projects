ac7832e9bc627bcff1f69e27abf7260de5245d5d
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-10 11:25:39 +0000

    Sat, Sep 10, 2022, 11:25 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -17,7 +17,6 @@ int main(void)
     // Score both words
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
-    printf("%s %lu",word1,strlen(word1));
     // TODO: Print the winner
 }
 
