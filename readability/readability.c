1b8e8d26f84f26747f4a90e54432314514abc957
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-11 22:06:12 +0000

    Sun, Sep 11, 2022, 10:06 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -2,17 +2,23 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#define isalphap(char c) (isalpha(c) && 1)
+int letters(string message);
 
 int main(void)
 {
     string message = get_string("Text: "); // getting an input from user
+    printf("letters is %i",letters(string message));
 }
 
-int letters(string message) //searching for letters in message
+int letters(string message) // searching for letters in message
 {
     int n = 0;
-    for (int i = 0; i < strlen(message); i++){
-        if()
-
+    for (int i = 0; i < strlen(message); i++)
+    {
+        if (isalphap(message[i]))
+        {
+            n++;
+        }
     }
 }
