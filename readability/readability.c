194851debcb807a1691ebb1a2a1b6c3868d868ef
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-11 22:03:33 +0000

    Sun, Sep 11, 2022, 10:03 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
-int main(void) 
+int main(void)
 {
+    string message = get_string("Text: "); // getting an input from user
+}
 
+int letters(string message) //searching for letters in message
+{
+    int n = 0;
+    for (int i = 0; i < strlen(message); i++){
+        if()
+
+    }
 }
