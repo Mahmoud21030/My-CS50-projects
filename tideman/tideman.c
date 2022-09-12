b12c1bc73a6b6fa096a5d166a66dd9289b565406
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-12 13:23:56 +0000

    Mon, Sep 12, 2022, 1:23 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -145,7 +145,7 @@ void add_pairs(void)
                     pairs[counter].winner=rows;
                     pairs[counter].loser=columns;
                     counter++;
-                    printf("counter is %i\n winner is %i\n loser is %i",counter,rows,columns);
+                    printf("counter is %i\n winner is %i\n loser is %i\n",counter,rows,columns);
                 }
             }
         }
