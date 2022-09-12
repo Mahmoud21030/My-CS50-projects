f903a426eb6de2259227d63cea6b86bb4c57cfe5
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-12 00:23:00 +0000

    Mon, Sep 12, 2022, 12:23 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -80,9 +80,9 @@ bool vote(string name)
 void print_winner(void)
 {
     int index = 0;
-    for (int i = 0; i <)
+    for (int i = 0; i < candidate_count ;i++)
     {
-        for (int j = 0; j < candidate_count - 1)
+        for (int j = 0; j < candidate_count - 1;j++)
         {
             if (candidates[i].votes >= candidates[i + 1].votes)
             {
