5f236ed65c1086310a783624c63b87f48b0d6ad5
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-12 12:07:27 +0000

    Mon, Sep 12, 2022, 12:07 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -103,7 +103,8 @@ bool vote(int rank, string name, int ranks[])
     {
         if (!strcmp(name, candidates[i]))
         {
-            ;
+            ranks[i]=i;
+            printf("%s",ranks);
             return true;
         }
     }
