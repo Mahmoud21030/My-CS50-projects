a31b89a36ef42bfa656d29288a13ddae6811e5e9
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-12 00:31:59 +0000

    Mon, Sep 12, 2022, 12:31 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -79,7 +79,7 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    int max = 0;
+    int max = -1;
     for (int i = 0; i < candidate_count ;i++)
     {
         for (int j = 0; j < candidate_count - 1;j++)
