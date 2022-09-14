8f524f3d2d98b473223cbacdbdc17df2554b4b50
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-14 12:55:43 +0000

    Wed, Sep 14, 2022, 12:55 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -6,7 +6,9 @@ typedef uint8_t Byte;
 int check(Byte array[]);
 
 int main(int argc, char *argv[])
-{   Byte array[512];
+{
+    int counter = 0;
+    Byte array[512];
     if (argc != 2)
     {
         printf("Usage: ./recover IMAGE");
@@ -14,18 +16,20 @@ int main(int argc, char *argv[])
     }
 
     FILE *input = fopen(argv[1], "r");
-    FILE *output;
-    while (fread(array,512,1,input))
+    FILE *output = fopen("First", "write");
+    while (fread(array, 512, 1, in))
     {
-      fclose(output);
-
-
+        if (check(array))
+        {
+            fclose(output);
+            
+        }
     }
 }
 
 int check(Byte array[])
 {
-    if(array[0]==0xff && array[1]==0xd8 && array[2]== 0xff && (array[3] & 0xf0)==0xe0)
-    return 1;
+    if (array[0] == 0xff && array[1] == 0xd8 && array[2] == 0xff && (array[3] & 0xf0) == 0xe0)
+        return 1;
     return 0;
 }
