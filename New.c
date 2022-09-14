d454a0859bd8968377383f87270e89817fc32f22
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-14 14:53:16 +0000

    Wed, Sep 14, 2022, 2:53 PM +00:00

--- New.c
+++ New.c
@@ -3,8 +3,9 @@
 
 int
  main(){
+    int array[3]={1,2,3}
     double   x =4.20;
      int z= x*200;
-    printf("%i\n",z);
+    printf("%s",array[4]);
 }
-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
+//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
