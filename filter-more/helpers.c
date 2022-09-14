daf9db823643d3c3df85abae517f55d5ecc306f2
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-14 14:21:19 +0000

    Wed, Sep 14, 2022, 2:21 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -24,6 +24,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    
     return;
 }
 
