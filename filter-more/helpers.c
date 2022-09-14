ad290cd67df06796a49c94dfdefe51f21ba54fb8
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-14 14:11:35 +0000

    Wed, Sep 14, 2022, 2:11 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -1,9 +1,23 @@
 #include "helpers.h"
-
+// BYTE  rgbtBlue;
+//  BYTE  rgbtGreen;
+//  BYTE  rgbtRed;
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    int temp = 0;
+    for (int rows = 0; rows < height, rows++)
+    {
+        for (int column = 0; column < width, column++)
+        {
+            temp = (image[rows][column].rgbtBlue +
+                    image[rows][column].rgbtRed +
+                    image[rows][column].rgbtGreen)/3 ;
+            image[rows][column].rgbtBlue=temp;
+            image[rows][column].rgbtRed=temp;
+            image[rows][column].rgbtGreen=temp;
+        }
+    }
     return;
 }
 
