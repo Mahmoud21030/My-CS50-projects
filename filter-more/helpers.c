94ecfe04eb6426f08d6376ac38ae2b1385aa1628
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-15 00:30:47 +0000

    Thu, Sep 15, 2022, 12:30 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -163,6 +163,24 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                                             copy[rows - 1][column-1].rgbtGreen+ //up left
                                             copy[rows -1][column+1].rgbtGreen)/6; //up right
             }
+            else if ((column == 0 && rows == 0 )
+            {    image[rows][column].rgbtBlue =
+                                            copy[rows ][column+1].rgbtBlue+//side
+                                            copy[rows +1][column].rgbtBlue+ //down
+                                            copy[rows +1][column+1].rgbtBlue)/3; //down side
+
+                 image[rows][column].rgbtRed = (
+                                            copy[rows ][column+1].rgbtRed+//side
+                                            copy[rows +1][column].rgbtRed+ //down
+
+                                            copy[rows +1][column+1].rgbtRed)/3; //down side
+                 image[rows][column].rgbtGreen = (
+                                            copy[rows ][column+1].rgbtGreen+//side
+                                            copy[rows +1][column].rgbtGreen+ //down
+                                            
+                                            copy[rows +1][column+1].rgbtGreen)/6; //down side
+
+            }
         }
     }
     return;
