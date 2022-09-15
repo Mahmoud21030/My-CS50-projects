e21ba619fa9a77f710df1f19e6e79221fdc74d19
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-15 10:54:13 +0000

    Thu, Sep 15, 2022, 10:54 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -347,31 +347,31 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                 }
 
                 x++;
-                if ((BYTE)round(sqrt(sqr(gx_s.rgbtGreen) + sqr(gy_s.rgbtGreen))) > 255)
+                if (round(sqrt(sqr(gx_s.rgbtGreen) + sqr(gy_s.rgbtGreen))) > 255)
                 {
-                    image[rows][columns].rgbtGreen = 255;
+                    image[rows][columns].rgbtGreen = 0xff;
                 }
                 else
                 {
-                    image[rows][columns].rgbtGreen = (BYTE)round(sqrt(sqr(gx_s.rgbtGreen) + sqr(gy_s.rgbtGreen)));
+                    image[rows][columns].rgbtGreen = round(sqrt(sqr(gx_s.rgbtGreen) + sqr(gy_s.rgbtGreen)));
                 }
 
-               if ((BYTE)round(sqrt(sqr(gx_s.rgbtRed) + sqr(gy_s.rgbtRed))) > 255)
+               if (round(sqrt(sqr(gx_s.rgbtRed) + sqr(gy_s.rgbtRed))) > 255)
                 {
-                    image[rows][columns].rgbtRed = 255;
+                    image[rows][columns].rgbtRed = 0xff;
                 }
                 else
                 {
-                    image[rows][columns].rgbtRed = (BYTE)round(sqrt(sqr(gx_s.rgbtRed) + sqr(gy_s.rgbtRed)));
+                    image[rows][columns].rgbtRed = round(sqrt(sqr(gx_s.rgbtRed) + sqr(gy_s.rgbtRed)));
                 }
 
-                if ((BYTE)round(sqrt(sqr(gx_s.rgbtBlue) + sqr(gy_s.rgbtBlue) )) > 255)
+                if (round(sqrt(sqr(gx_s.rgbtBlue) + sqr(gy_s.rgbtBlue) )) > 255)
                 {
-                    image[rows][columns].rgbtBlue = 255;
+                    image[rows][columns].rgbtBlue = 0xff;
                 }
                 else
                 {
-                    image[rows][columns].rgbtBlue = (BYTE)round(sqrt(sqr(gx_s.rgbtBlue) + sqr(gy_s.rgbtBlue)));
+                    image[rows][columns].rgbtBlue = round(sqrt(sqr(gx_s.rgbtBlue) + sqr(gy_s.rgbtBlue)));
                 }
             }
             gx_s.rgbtRed = 0;
