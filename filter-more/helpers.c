4657245af21167a647d39cd26f911b5dda93fb6f
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-15 01:46:43 +0000

    Thu, Sep 15, 2022, 1:46 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -242,20 +242,20 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             else if (column == (width -1) && rows == (height -1) )
             {    image[rows][column].rgbtBlue =
                                             round((image[rows][column].rgbtBlue+
-                                            copy[rows][column+1].rgbtBlue+//side
+                                            copy[rows][column-1].rgbtBlue+//side
                                             copy[rows -1][column].rgbtBlue+ //down
                                             copy[rows -1][column+1].rgbtBlue)/4.0); //down side
 
                  image[rows][column].rgbtRed = round((image[rows][column].rgbtRed+
-                                            copy[rows ][column+1].rgbtRed+//side
+                                            copy[rows ][column-1].rgbtRed+//side
                                             copy[rows -1][column].rgbtRed+ //down
 
-                                            copy[rows -1][column+1].rgbtRed)/4.0); //down side
+                                            copy[rows -1][column-1].rgbtRed)/4.0); //down side
                  image[rows][column].rgbtGreen = round((image[rows][column].rgbtGreen+
-                                            copy[rows ][column+1].rgbtGreen+//side
+                                            copy[rows ][column-1].rgbtGreen+//side
                                             copy[rows -1][column].rgbtGreen+ //down
 
-                                            copy[rows -1][column+1].rgbtGreen)/4.0); //down side
+                                            copy[rows -1][column-1].rgbtGreen)/4.0); //down side
 }
 
         }
