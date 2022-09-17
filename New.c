8c2e04945cd0bfc503f30914314827fedd59b3a5
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-17 14:01:37 +0000

    Sat, Sep 17, 2022, 2:01 PM +00:00

--- New.c
+++ New.c
@@ -4,11 +4,11 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <ctype.h>
-
+#include <string.h>
 
 typedef struct ST_cardData_t
 {
-	uint8_t cardHolderName[25];
+	char cardHolderName[25];
 	uint8_t primaryAccountNumber[20];
 	uint8_t cardExpirationDate[6];
 }ST_cardData_t;
@@ -26,15 +26,15 @@ EN_cardError_t getCardExpiryDate(ST_cardData_t* cardData);
 EN_cardError_t getCardPAN(ST_cardData_t* cardData);
 EN_cardError_t getCardHolderName(ST_cardData_t* cardData)
 {
-	uint8_t temp[25];
+	char temp[25];
 	printf("**Please Enter The CardHolder Name: ");
-	gets(temp);
+	scanf("%s",temp);
 
 	/* Checking if the length of the string is between 19 and 25 characters. If it is, it assigns the
 	adress of temp to cardData->cardHolderName. */
 	if (strlen(temp) > 19 && strlen(temp) < 25)
 	{
-		cardData->cardHolderName= temp;
+		cardData->(cardHolderName= temp;
 		printf("error %s", temp);
 		return OK;
 	}
