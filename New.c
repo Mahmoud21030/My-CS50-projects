591061e87b86596ce3f4269a153cc57133ab8dcb
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-17 13:48:57 +0000

    Sat, Sep 17, 2022, 1:48 PM +00:00

--- New.c
+++ New.c
@@ -5,15 +5,23 @@
  *
  *  cardData a pointer to a structure of type ST_cardData_t.
  */
-EN_cardError_t getCardHolderName(ST_cardData_t* cardData)
+/**
+ * It returns 1 if all the characters in the string are alphabetic, and 0 otherwise
+ *
+ * @param string The string to be checked.
+ *
+ * @return the number of characters in the string that are alphabetic.
+ */
+int s_isalpha(char* string)
 {
-	char temp[25];
-	printf("**Please Enter The CardHolder Name ");
-	gets(temp);
-	/* Checking if the length of the string is between 19 and 25 characters. If it is, it assigns the
-	value of temp to cardData->cardHolderName. */
-	if (strlen(temp) > 19 && strlen(temp) < 25)
-		cardData->cardHolderName = temp;
-		printf("error %s %i", temp);
+	int n = strlen(string);
+	int counter = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (isalpha(string[i]) != 0)
+		counter++;
+	}
+	return counter == n;
+}
 
 //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
