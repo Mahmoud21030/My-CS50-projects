07ec918765151773a38e2858596b4dbc6f19ab98
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-17 21:09:03 +0000

    Sat, Sep 17, 2022, 9:09 PM +00:00

--- New.c
+++ New.c
@@ -54,5 +54,41 @@ uint8_t* CardGenerator(int x)
 	return final;
 }
 
+/**
+ * The function checks if the credit card number is valid or not.
+ *
+ * @param string a string of numbers
+ *
+ * @return The function isValidCard() returns the first digit of the sum of the digits of the card
+ * number.
+ */
+int isValidCard(char* string)
+{
+	int n = strlen(string);
+
+	uint8_t* array = calloc(4, n);
+	for (int i = 0; i < n; i++) //convert from string to array of uint8_t
+	{
+		array[i] = string[i] - '0';
+	}
+	int sum = 0;
+	for (int i = n - 1; i >= 0; i -= 2) // sum of number start from the last index and step 2
+		sum += array[i];
+	for (int i = n - 2; i >= 0; i -= 2)
+		/* sum of 2 * number start from the digit next to the last
+		index and step 2*/
+	{
+		if ((2 * array[i]) > 9)
+		{
+			sum += (2 * array[i]) % 10 + (2 * array[i]) / 10;
+		}
+		else
+		{
+			sum += 2 * array[i];
+		}
+	}
+	free(array);
+	return (sum % 10); /*if the frist digit of sum is 0 then output is 0 value otherwise non zero number*/
+}
 
 //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
