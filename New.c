0059ac168da70fb3ad22f872050deefd6e09c0c9
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-17 11:58:58 +0000

    Sat, Sep 17, 2022, 11:58 AM +00:00

--- New.c
+++ New.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
+int main()
+{
+	// variables in which to store data
+	char name[15];
+	char unit[15];
+	int amount;
 
-int
- main(){
-    int array[3]={1,2,3}
-    double   x =4.20;
-     int z= x*200;
-    printf("%s",array[4]);
+	// taking input and storing in variables
+	// a sampleinput string would be:
+	// "Faraz owns 500 acres of land"
+#ifdef __STDC_LIB_EXT1__ // only use swscanf_s if __STDC_LIB_EXT1__ is already defined
+	scanf_s("%s %*s %d %s", name, &amount, unit);
+#endif
+	// use normal scanf_s
+	scanf_s("%s %*s %d %s", name, &amount, unit);
+	// print out new string using the extracted values
+	printf("% d %s of land is owned by %s\n", amount, unit, name);
+
+	return 0;
 }
 //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
