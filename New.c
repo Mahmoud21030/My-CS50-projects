5db41a9139d63ce68a654bdc0ca02088eea33b87
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-17 17:52:19 +0000

    Sat, Sep 17, 2022, 5:52 PM +00:00

--- New.c
+++ New.c
@@ -1,70 +1,27 @@
 
-#include <stdio.h>
-#include <stdint.h>
-#include <ctype.h>
 #include <string.h>
-int main(){
+#include <stdint.h>
+#include <stdio.h>
 
-}
-typedef struct ST_cardData_t
+int main()
 {
-	char cardHolderName[25];
-	uint8_t primaryAccountNumber[20];
-	uint8_t cardExpirationDate[6];
-}ST_cardData_t;
+	//ST_cardData_t user1;
+	//getCardHolderName(&user1);
+	//getCardPAN(&user1);
+	//getCardExpiryDate(&user1);
+	//
 
-typedef enum EN_cardError_t
-{
-	OK,
-	WRONG_NAME,
-	WRONG_EXP_DATE,
-	WRONG_PAN
-}EN_cardError_t;
-
-EN_cardError_t getCardHolderName(ST_cardData_t* cardData);
-EN_cardError_t getCardExpiryDate(ST_cardData_t* cardData);
-EN_cardError_t getCardPAN(ST_cardData_t* cardData);
-
-EN_cardError_t getCardHolderName(ST_cardData_t* cardData)
-{
-	char temp[25];
-	printf("**Please Enter The CardHolder Name: ");
-	scanf("%s",cardData->cardHolderName);
-
-	/* Checking if the length of the string is between 19 and 25 characters. If it is, it assigns the
-	adress of temp to cardData->cardHolderName. */
-	if (strlen(temp) > 19 && strlen(temp) < 25)
-	{
-		cardData->cardHolderName= temp;
-		printf("error %s", temp);
-		return OK;
-	}
-	else
-	{
-		return WRONG_NAME;
-	}
-}
-/**
- * It checks if the string is a date in the format dd/mm/yyyy
- *
- * @param string The string to be checked.
- *
- * @return 1 if the format is correct, 0 if not
- */
-int s_isdate(char* string)
-{
+	char string[] = "123456789";
 	int n = strlen(string);
-	int counter = 0;
-	int month = atoi(string); //taking month to integer
-	for (int i = 0; i < n; i++) //checking the format
+	uint8_t array[strlen(string)];
+	for (int i = 0; i < n; i++)
 	{
-		if (isdigit(string[i]) != 0)
-			counter++; //counter is the number of digits in format
+		array[i] = string[i] - '0';
+			printf("%i",array[i]);
 	}
-	if (counter == 4 && string[2] == '/' && month <13)
-		return 1;
 
-	return 0;
+
 }
+
 
 //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
