b3376ebaaf0cf4c27fe79c92aef7d83b9f14e6f0
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-17 01:36:05 +0000

    Sat, Sep 17, 2022, 1:36 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -49,7 +49,7 @@ bool load(const char *dictionary)
 
     while (!feof(file))
     {
-        node *n = calloc(4,sizeof(node)/4);
+        node *n = calloc(1,sizeof(node));
         if (n == NULL)
         {
             return false;
