4c16494f4a0348a262415544c17a7b9246a264ef
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-17 12:31:21 +0000

    Sat, Sep 17, 2022, 12:31 PM +00:00

--- New.c
+++ New.c
@@ -10,6 +10,8 @@ EN_cardError_t getCardHolderName(ST_cardData_t* cardData)
 	char temp[25];
 	printf("**Please Enter The CardHolder Name ");
 	gets(temp);
+/* Checking the length of the string and if it is less than 20 or greater than 24 it will print an
+error. */
 	if(strlen(temp)<20 && strlen(temp) > 24)
 		printf("error %s ", temp);
 
