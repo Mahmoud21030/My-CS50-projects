bfbdac4151a6c6ef5aa3c56d4baa31b155d77f32
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-24 11:29:44 +0000

    Sat, Sep 24, 2022, 11:29 AM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,19 +1,21 @@
 import sys
+
+
 def main():
     Height = int(input("Height: "))
-    buildp(Height,Height)
+    buildp(Height, Height)
 
 
-def buildp(number,loop):
+def buildp(number, loop):  # recursive func.
     if number == 0:
         return
-    print(" " * (number-1), end="")
+    print(" " * (number-1), end="")  # the sape we want
     print("#"*(loop-number+1), end="")
-    print(" ",end="")
+    print(" ", end="")
     print("#"*(loop-number+1), end="")
     print(" " * (number-1))
-    buildp(number-1,loop)
+    buildp(number-1, loop)
 
-
-if __name__=="__main__":
+ # running the main
+if __name__ == "__main__":
     main()
