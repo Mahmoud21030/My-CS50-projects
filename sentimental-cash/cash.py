91836084fdc4300880dfce0a6f28d2d1129d95ef
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-24 12:40:42 +0000

    Sat, Sep 24, 2022, 12:40 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -2,17 +2,17 @@ from cs50 import get_float
 from math import floor
 
 change = 100*get_float("Change owed: ")
+if(change > 0):
+    quarter = int(change / 25.0)
+    change = change -quarter *25
 
-quarter = int(change / 25.0)
-change = change -quarter *25
+    dime = int(change /10.0)
+    change = change -dime *10
 
-dime = int(change /10.0)
-change = change -dime *10
+    nickels =int(change/5)
+    pennies = change -nickels*5
 
-nickels =int(change/5)
-pennies = change -nickels*5
-
-print(quarter + dime + nickels + pennies)
+    print(quarter + dime + nickels + pennies)
 
 
 
