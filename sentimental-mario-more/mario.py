aae64541c487f8a9e1b2f6dcfb41f18df479cb50
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-24 11:38:12 +0000

    Sat, Sep 24, 2022, 11:38 AM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -2,8 +2,11 @@ import sys
 
 
 def main():
-    Height = int(input("Height: "))
-    buildp(Height, Height)
+    Height = get_height()
+    if(Height <1):
+        print("Height must be higher than 0")
+    else:
+        buildp(Height, Height)
 
 
 def buildp(number, loop):  # recursive func.
@@ -16,6 +19,13 @@ def buildp(number, loop):  # recursive func.
     print(" " * (number-1))
     buildp(number-1, loop)
 
+def get_height():
+    while True:
+        try:
+            Height = int(input("Height: "))
+            return Height
+        except ValueError:
+            print("Enter valid value")
  # running the main
 if __name__ == "__main__":
     main()
