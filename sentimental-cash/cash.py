531cc6df2661c08b591ebd2711da5ac10192e284
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-24 12:46:09 +0000

    Sat, Sep 24, 2022, 12:46 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,18 +1,21 @@
 from cs50 import get_float
 from math import floor
 
-change = 100*get_float("Change owed: ")
-if(change > 0):
-    quarter = int(change / 25.0)
-    change = change -quarter *25
+change = 0
+# taking the valid input
+while (change < 1):
+    try:
+        change = 100*get_float("Change owed: ")
+    except ValueError:
+        print("invalid input")
 
-    dime = int(change /10.0)
-    change = change -dime *10
+quarter = int(change / 25.0)  # getting quarter
+change = change - quarter * 25
 
-    nickels =int(change/5)
-    pennies = change -nickels*5
+dime = int(change / 10.0)  # dimes
+change = change - dime * 10
 
-    print(quarter + dime + nickels + pennies)
+nickels = int(change/5)
+pennies = change - nickels*5  # pennies is the rest
 
-
-
+print(int(quarter + dime + nickels + pennies))  # sum of them all
