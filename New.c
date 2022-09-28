06ccff85d0e6ea9b9c74f84fa939421d6d18428b
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-09-28 00:16:08 +0000

    Wed, Sep 28, 2022, 12:16 AM +00:00

--- New.c
+++ New.c
@@ -9,16 +9,65 @@
 /*			                  	MCU-Layer	             	 		     */
 /************************************************************************/
 
-/**
- * It takes in a port, pin, and data, and then sets the pin to the data
- *
- * @param PORT The port you want to write to.
- * @param pin The pin number you want to write to.
- * @param data 1 or 0
- */
 #include "MCU.h"
 
-void D_write(uint8_t PORT, uint8_t pin, uint8_t data)
+void inti(uint8_t PORT, uint8_t pin, bool dirc)
+{
+  switch (PORT)
+  {
+  case PORT_A:
+    if (dirc == 1)
+    {
+      set_bit(DDRA, pin);
+    }
+    else if (dirc == 0)
+    {
+      clear_bit(DDRA, pin);
+    }
+    break;
+
+  case PORT_B:
+    if (dirc == 1)
+    {
+      set_bit(DDRB, pin);
+    }
+    else if (dirc == 0)
+    {
+      clear_bit(DDRB, pin);
+    }
+    break;
+
+  case PORT_C:
+    if (dirc == 1)
+    {
+      set_bit(DDRC, pin);
+    }
+    else if (dirc == 0)
+    {
+      clear_bit(DDRC, pin);
+    }
+    break;
+
+  case PORT_D:
+    if (dirc == 1)
+    {
+      set_bit(DDRD, pin);
+    }
+    else if (dirc == 0)
+    {
+      clear_bit(DDRD, pin);
+    }
+    break;
+  }
+}
+
+/**
+ * It takes in a port, pin, and data, and then sets the pin to the data
+ *  PORT The port you want to write to.
+ *  pin The pin number you want to write to.
+ *  data 1 or 0
+ */
+void D_write(uint8_t PORT, uint8_t pin, bool data)
 {
   switch (PORT)
   {
@@ -64,6 +113,35 @@ void D_write(uint8_t PORT, uint8_t pin, uint8_t data)
     {
       clear_bit(PORTD, pin);
     }
+    break;
+  }
+}
+
+/**
+ * It reads the value of a pin on a port and stores it in a variable
+ *
+ * @param PORT The port you want to read from.
+ * @param pin The pin you want to read from.
+ * @param dest The destination variable to store the read value in.
+ */
+void D_read(uint8_t PORT, uint8_t pin, uint8_t *dest)
+{
+  switch (PORT)
+  {
+  case PORT_A:
+    *dest = (PINA && (1 << pin)) >> pin;
+    break;
+
+  case PORT_B:
+    *dest = (PINB && (1 << pin)) >> pin;
+    break;
+
+  case PORT_C:
+    *dest = (PINC && (1 << pin)) >> pin;
+    break;
+
+  case PORT_D:
+    *dest = (PIND && (1 << pin)) >> pin;
     break;
   }
 }
---
+++ dna.py
@@ -0,0 +1,83 @@
+import csv
+import sys
+
+
+def main():
+
+    compare = []  # our database
+
+    if len(sys.argv) != 3:  # check for commandline
+        sys.exit("Usage: python dna.py data.csv sequence.txt")
+        # getting files' names
+    database = sys.argv[1]
+    txtfile = sys.argv[2]
+
+    with open(txtfile, "r") as s_file:
+        sequence = s_file.read()
+
+    with open(database, "r") as t_file:
+        table = csv.DictReader(t_file)
+        for data in table:
+            compare.append(data)
+    # print(compare["AGATC"])
+
+    #  Find longest match of each STR in DNA sequence
+
+    keys = list(compare[0].keys())[1:]
+    results = {}
+    for i in keys:
+        results[i] = longest_match(sequence, i)
+       # print(results)
+    counter = 0
+    for j in range(len(compare)):
+        counter = 0
+        for i in keys:
+            if (int(compare[j][i]) == int(results[i])):
+                counter += 1
+                if (counter == len(keys)):
+                    print(compare[j]["name"])
+                    sys.exit()
+    print("No match")
+
+    return
+
+
+def longest_match(sequence, subsequence):
+    """Returns length of longest run of subsequence in sequence."""
+
+    # Initialize variables
+    longest_run = 0
+    subsequence_length = len(subsequence)
+    sequence_length = len(sequence)
+
+    # Check each character in sequence for most consecutive runs of subsequence
+    for i in range(sequence_length):
+
+        # Initialize count of consecutive runs
+        count = 0
+
+        # Check for a subsequence match in a "substring" (a subset of characters) within sequence
+        # If a match, move substring to next potential match in sequence
+        # Continue moving substring and checking for matches until out of consecutive matches
+        while True:
+
+            # Adjust substring start and end
+            start = i + count * subsequence_length
+            end = start + subsequence_length
+
+            # If there is a match in the substring
+            if sequence[start:end] == subsequence:
+                count += 1
+
+            # If there is no match in the substring
+            else:
+                break
+
+        # Update most consecutive matches found
+        longest_run = max(longest_run, count)
+
+    # After checking for runs at each character in seqeuence, return longest run found
+    return longest_run
+
+
+main()
