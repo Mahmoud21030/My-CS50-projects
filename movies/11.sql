ec26f18fedafde5ca6ce7996cf8a5dbaf5c9f453
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-10-07 19:27:10 +0000

    Fri, Oct 7, 2022, 7:27 PM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,3 +1,3 @@
 SELECT  title  FROM movies JOIN (stars JOIN people ON stars.person_id = people.id) ON movies.id = stars.movie_id
 JOIN ratings ON movies.id = ratings.movie_id
-WHERE people.name = "Chadwick Boseman" ORDER BY ratings.rating LIMIT 5;
+WHERE people.name = "Chadwick Boseman" ORDER BY ratings.rating DESC LIMIT 5 ;
