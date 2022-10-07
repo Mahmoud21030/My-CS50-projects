d74ec8f8afee5d185cf9c0b7f7a1d7229a61f347
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-10-07 19:09:21 +0000

    Fri, Oct 7, 2022, 7:09 PM +00:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,2 +1,2 @@
-SELECT  DISTINCT(name)  FROM movies JOIN (directors JOIN people ON directors.person_id = people.id) ON movies.id = directors.movie_id
-WHERE id IN (SELECT movie_id FROM ratings WHERE  rating > 9);
+SELECT  COUNT(DISTINCT(name))  FROM movies JOIN (directors JOIN people ON directors.person_id = people.id) ON movies.id = directors.movie_id
+WHERE movies.id IN (SELECT movie_id FROM ratings WHERE (rating = 9 OR rating > 9));
