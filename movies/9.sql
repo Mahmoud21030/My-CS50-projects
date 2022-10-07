4c8fbdd8880d836249ebfacf8444a0129a46a18a
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-10-07 18:48:12 +0000

    Fri, Oct 7, 2022, 6:48 PM +00:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,2 +1,2 @@
-SELECT  name  FROM movies JOIN (stars JOIN people ON stars.person_id = people.id) ON movies.id = stars.movie_id
+SELECT  DISTINCT(name)  FROM movies JOIN (stars JOIN people ON stars.person_id = people.id) ON movies.id = stars.movie_id
 WHERE year = 2004 ORDER BY birth ;
