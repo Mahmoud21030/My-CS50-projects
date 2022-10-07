84d67a3ab67dccad970ea15a8142efac65b3c93e
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-10-07 19:31:34 +0000

    Fri, Oct 7, 2022, 7:31 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,2 +1,2 @@
 SELECT  title ,name FROM movies JOIN (stars JOIN people ON stars.person_id = people.id) ON movies.id = stars.movie_id
-WHERE people.name = "Johnny Depp" AND people.name = "Helena Bonham";
+WHERE people.name = "Johnny Depp" OR people.name = "Helena Bonham";
