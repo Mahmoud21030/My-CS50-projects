8a43f508b18228c44683e92c6dc6f166ebd904ec
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-10-07 18:58:50 +0000

    Fri, Oct 7, 2022, 6:58 PM +00:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,1 +1,2 @@
-SELECT rating ,title  From movies JOIN ratings ON movies.id = ratings.movie_id ORDER BY rating DESC,title  ;
+SELECT rating ,title  From movies JOIN ratings ON movies.id = ratings.movie_id
+WHERE year = 2010 ORDER BY rating DESC,title  ;
