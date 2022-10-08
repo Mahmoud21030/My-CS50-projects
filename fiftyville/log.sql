ff2bf69d7ca8a4bf781c53c14b2309f094a73fb6
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-10-08 15:05:50 +0000

    Sat, Oct 8, 2022, 3:05 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,3 +3,4 @@
 --atm_transactions      flights               phone_calls
 --bakery_security_logs  interviews
 --bank_accounts         passengers
+sqlite> .schema crime_scene_reports -- the structure of the crime_scene_reports
