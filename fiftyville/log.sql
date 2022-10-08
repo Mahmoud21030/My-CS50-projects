f9430025c2df4b82ba2a510e532ad1348d7b9f66
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-10-08 15:04:52 +0000

    Sat, Oct 8, 2022, 3:04 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
-
+--airports              crime_scene_reports   people
+--atm_transactions      flights               phone_calls
+--bakery_security_logs  interviews
+--bank_accounts         passengers
