6f45b1474da9b8a67f4212a9b8a4eacfdeb81f3b
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-10-08 16:09:09 +0000

    Sat, Oct 8, 2022, 4:09 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -145,10 +145,11 @@ AND phone_number IN (SELECT caller FROM phone_calls  WHERE year = 2021 AND month
 --| Diana  | (770) 555-1861 |
 --| Bruce  | (367) 555-5533 |
 --------+----------------+
-SELECT passport_number FROM people WHERE name IN
+SELECT * FROM passengers WHERE passport_number IN
+(SELECT passport_number FROM people WHERE name IN
 (SELECT name FROM people WHERE id IN
 (SELECT person_id FROM bank_accounts WHERE account_number IN
 ( SELECT account_number FROM atm_transactions  WHERE year = 2021 AND month = 7 AND day =28 AND atm_location LIKE "Leggett Street"  AND transaction_type = "withdraw")))
 AND name IN (SELECT name FROM people WHERE license_plate IN
 (SELECT  license_plate FROM bakery_security_logs  WHERE year = 2021 AND month = 7 AND day =28 AND hour = 10  AND minute >=16 AND activity = "exit"))
-AND phone_number IN (SELECT caller FROM phone_calls  WHERE year = 2021 AND month = 7 AND day =28 AND duration < 60);
+AND phone_number IN (SELECT caller FROM phone_calls  WHERE year = 2021 AND month = 7 AND day =28 AND duration < 60));
