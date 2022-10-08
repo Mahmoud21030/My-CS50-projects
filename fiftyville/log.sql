f83bbf39031070ad3dabba61a409887a548ef2ee
Mahmoud21030 <111393894+Mahmoud21030@users.noreply.github.com>
2022-10-08 15:10:34 +0000

    Sat, Oct 8, 2022, 3:10 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -4,3 +4,5 @@
 --bakery_security_logs  interviews
 --bank_accounts         passengers
 sqlite> .schema crime_scene_reports -- the structure of the crime_scene_reports
+SELECT * FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day =28 ; --info from data 
+
